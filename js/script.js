9884e288d3e7c01a00edcbf37194ab3090a8230a
Evgeny <evgbut55@gmail.com>
2021-06-04 21:24:44 -0400

    3

--- js/script.js
+++ js/script.js
@@ -37,8 +37,64 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     })     
 
+// Timer:
 
+let deadline = '2021-06-20';
 
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+    seconds = Math.floor( (t/1000) % 60 ),
+    minutes = Math.floor( (t/1000/60) % 60),
+    hours = Math.floor( (t/(1000*60*60)) );
 
+    return {
+        'total': t,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+};
+
+function setClock(id, endtime) {
+
+     let timer = document.getElementById(id),
+         hours = timer.querySelector('.hours'),
+         minutes = timer.querySelector('.minutes'),
+         seconds = timer.querySelector('.seconds');
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        };
+
+        updateClock();
+        let timeInterval = setInterval(updateClock, 1000);
+};
+
+setClock('timer', deadline);
+
+// Modal:
+
+let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        this.classList.add('more-splash');
+        overlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 
 });
