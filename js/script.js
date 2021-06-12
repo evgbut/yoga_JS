679e7aa47b0476f5a5fa950e54dc8d1e73094a48
Evgeny <evgbut55@gmail.com>
2021-06-12 19:57:17 -0400

    4

--- js/script.js
+++ js/script.js
@@ -97,4 +97,51 @@ let more = document.querySelector('.more'),
         document.body.style.overflow = '';
     });
 
+// Form:
+
+    let message = new Object();
+    message.loading = "Загрузка...";
+    message.success = "Спасибо! Скоро мы с вами свяжемся";
+    message.failure = "Что-то пошло не так...";
+
+    let form = document.getElementsByClassName('main-form')[0],
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        // AJAX:
+        let request = new XMLHttpRequest();
+        request.open("POST", 'server.php');
+
+        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        let formData = new FormData(form);
+
+        request.send(formData);
+
+        request.onreadystatechange = function() {
+            if(request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4) {
+                if(request.status == 200 && request.status < 300) {
+                    statusMessage.innerHTML = message.success;
+                    // Добавляем контент на страницу
+                }
+                else {
+                    statusMessage.innerHTML = message.failure;
+ //                   console.log(request.status);
+                }
+            }
+        }
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+            // Очищаем поля ввода
+        }
+    });
+
 });
